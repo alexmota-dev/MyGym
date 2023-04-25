5347b484f6e2dab799dcfe854ab749f687102878
AlexxMotta <alexmota@alu.ufc.br>
2023-04-25 18:52:45 -0300

    Creating UserController adding first route, findAll.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGym.Models;
+
+namespace MyGym.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        public ActionResult<Task<List<UserModel>>> FindAll()
+        {
+            return Ok();
+        }
+    }
+}
