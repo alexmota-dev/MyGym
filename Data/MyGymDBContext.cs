c73f1f55fb8e2e95394f3ba31040d9d8cce28589
AlexxMotta <alexmota@alu.ufc.br>
2023-04-25 18:46:16 -0300

    Creating MyGymDBContext, introducing user

---
+++ Data/MyGymDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyGym.Models;
+
+namespace MyGym.Data
+{
+    public class MyGymDBContext : DbContext
+    {
+        public MyGymDBContext(DbContextOptions<MyGymDBContext>options):base(options)
+        {
+        }    
+
+        public DbSet<UserModel> Users { get; set; } 
+    }
+}
