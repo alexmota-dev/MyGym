41debe3bbbd982da15498a90e296c5b911afa8ac
AlexxMotta <alexmota@alu.ufc.br>
2023-04-25 18:38:24 -0300

    Creating interface user repository

---
+++ Repository/Interface/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyGym.Models;
+
+namespace MyGym.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        Task<List<UserModel>> GetAll();
+        Task<UserModel> GetById(int id);
+        Task<UserModel> Create(UserModel model);
+        Task<UserModel> Update(UserModel model, string id);
+        Task<UserModel> Delete(string id);
+    }
+}
