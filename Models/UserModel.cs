504f7cf158e346caca48a8f34d7049700af9da55
AlexxMotta <alexmota@alu.ufc.br>
2023-04-25 18:28:22 -0300

    creating user model

---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyGym.Models
+{
+    public class UserModel
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Avatar { get; set; }
+    }
+}
