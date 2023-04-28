7c7ec427ce65f061ea310a602be0cf8e277a69f1
AlexxMotta <alexmota@alu.ufc.br>
2023-04-28 14:43:17 -0300

    Refatoring InterfaceUserRepository, function delete is type bool and in GetById Id is string type.

--- Repository/Interface/IUserRepository.cs
+++ Repository/Interface/IUserRepository.cs
@@ -5,9 +5,9 @@ namespace MyGym.Repository.Interface
     public interface IUserRepository
     {
         Task<List<UserModel>> GetAll();
-        Task<UserModel> GetById(int id);
+        Task<UserModel> GetById(string id);
         Task<UserModel> Create(UserModel model);
         Task<UserModel> Update(UserModel model, string id);
-        Task<UserModel> Delete(string id);
+        Task<bool> Delete(string id);
     }
 }
