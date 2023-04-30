53bab25b29619e077784cfbe4b35b9efb416963c
AlexxMotta <alexmota@alu.ufc.br>
2023-04-30 16:36:27 -0300

    creating Startup class used in .Net 5

---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using MyGym.Data;
+using MyGym.Repository.Interface;
+using MyGym.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyGym
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //Conexão com o DB
+            builder.Services.AddDbContext<MyGymDBContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            //Injeção de dependência, UserRepository usa IUseRepository
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
